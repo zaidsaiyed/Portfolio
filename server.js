3ed14d67e1cfb39a89753c2582f45959cb0e7ffb
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2024-01-18 05:08:36 -0500

    Update console log message in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Hello World');
+    console.log('Hello World!');
     // link it to the index.html
     res.sendFile(__dirname + '/index.html');
     }
